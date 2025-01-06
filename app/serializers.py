62203eb7e960ca7b17857b7fb9523ff1405be85a
vinit gohil <vinit.gohil03@gmail.com>
2025-01-06 15:56:32 +0530

    Added Docker configuration for database

--- app/serializers.py
+++ app/serializers.py
@@ -1,17 +1,41 @@
+# from rest_framework import serializers
+# from .models import User, Subject, Mark
+
+# class MarkSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Mark
+#         fields = '__all__'
+
+# class UserSerializer(serializers.ModelSerializer):
+#     marks = MarkSerializer(many=True, read_only=True)
+
+#     class Meta:
+#         model = User
+#         fields = '__all__'
+
+# class SubjectSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Subject
+#         fields = '__all__'
 from rest_framework import serializers
 from .models import User, Subject, Mark
 
+
 class MarkSerializer(serializers.ModelSerializer):
+    subject_name = serializers.CharField(source='subject.name', read_only=True)  # Add subject name
+
     class Meta:
         model = Mark
-        fields = '__all__'
+        fields = ['id', 'marks', 'subject', 'subject_name', 'user']  # Include subject_name
+
 
 class UserSerializer(serializers.ModelSerializer):
-    marks = MarkSerializer(many=True, read_only=True)
+    marks = MarkSerializer(many=True, read_only=True)  # Use the updated MarkSerializer
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'name', 'email', 'role', 'marks']  # Include all necessary fields
+
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
--- app/views.py
+++ app/views.py
@@ -1,31 +1,70 @@
-from rest_framework import generics
+# from rest_framework import generics
+# from .models import User, Subject, Mark
+# from .serializers import UserSerializer, SubjectSerializer, MarkSerializer
+
+# class UserListCreateView(generics.ListCreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+# class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+#     def perform_destroy(self, instance):
+#         if instance.marks.exists():
+#             raise serializers.ValidationError("Cannot delete a user with marks.")
+#         instance.delete()
+
+# class SubjectListCreateView(generics.ListCreateAPIView):
+#     queryset = Subject.objects.all()
+#     serializer_class = SubjectSerializer
+
+# class SubjectDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Subject.objects.all()
+#     serializer_class = SubjectSerializer
+
+# class MarkListCreateView(generics.ListCreateAPIView):
+#     queryset = Mark.objects.all()
+#     serializer_class = MarkSerializer
+
+# class MarkDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Mark.objects.all()
+#     serializer_class = MarkSerializer
+from rest_framework import generics, status, exceptions
+from rest_framework.response import Response
 from .models import User, Subject, Mark
 from .serializers import UserSerializer, SubjectSerializer, MarkSerializer
+
 
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
     def perform_destroy(self, instance):
-        if instance.marks.exists():
-            raise serializers.ValidationError("Cannot delete a user with marks.")
+        if instance.marks.exists():  # Checks if the user has associated marks
+            raise exceptions.ValidationError("Cannot delete a user with marks.")
         instance.delete()
+
 
 class SubjectListCreateView(generics.ListCreateAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
+
 class SubjectDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
+
 class MarkListCreateView(generics.ListCreateAPIView):
     queryset = Mark.objects.all()
     serializer_class = MarkSerializer
+
 
 class MarkDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Mark.objects.all()
